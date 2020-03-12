c6b506f97940c837ef02810c746f470cdf13de67
dewi29setiyawati <dewi.setiyawati@students.amikom.ac.id>
2020-03-12 22:54:40 +0700

    rumus if else dan switch case

--- 19.11.2658.cs
+++ 19.11.2658.cs
@@ -22,6 +22,48 @@ namespace CalculatorConsoleApp
       int menu =int.Parse(Console.ReadLine());
       Console.WriteLine();
 
+      if(menu > 0 && menu < 5){
+        Console.Write("Input Nilai A = ");
+        int a = int.Parse(Console.ReadLine());
+        Console.Write("Input Nilai B = ");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        switch(menu){
+          case 1:
+            Console.WriteLine();
+            Console.WriteLine("Hasil Penambahan " + a + "+" + b + " = " + Penambahan(a, b));
+            break;
+          case 2:
+            Console.WriteLine();
+            Console.WriteLine("Hasil Pengurangan : {0} - {1} = {2}", a, b, Pengurangan(a, b));
+            break;
+          case 3:
+            Console.WriteLine();
+            Console.WriteLine("Hasil Perkalian : {0} * {1} = {2}", a, b, Perkalian(a, b)):
+            break;
+          case 4:
+            Console.WriteLine();
+            Console.WriteLine("Hasil Pembagian : {0} / {1} = {2}", a, b, Pembagian(a, b));
+            break;
+        }
+      }else{
+        Console.WriteLine("\nMaaf menu yang anda pilih tidak tersedia");
+      }
+      Console.WriteLine();
+      Console.WriteLine("\nTekan key apapun untuk keluar");
+      Console.ReadKey();
+    }
+    static int Penambahan(int a, int b){
+      return a + b;
+    }
+    static int Pengurangan(int a, int b){
+      return a - b;
+    }
+    static int Perkalian(int a, int b){
+      return a * b;
+    }
+    static int Pembagian(int a, int b){
+      return a / b;
     }
   }
 }
